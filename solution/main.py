23fbdc10db0820763c52928cc32c61686b69bdf8
Jeremiah Nofrada <jnnofrada@gmail.com>
2020-08-04 18:13:39 -0400

    more edge cases dealt with and better db connection handling

--- solution/main.py
+++ solution/main.py
@@ -35,9 +35,6 @@ def process_next_job():
     that in mind when designing your code.
 
     """
-    # open connection to db
-    db = llamadb.LlammaDB()
-    db.connect()
 
     """
     since this function is being continuously called only deal with one job per call
@@ -46,7 +43,11 @@ def process_next_job():
 
     """
     queue = queue.JobQueue()
-    job = queue.poll()
+    try:
+        job = queue.poll()
+    except IOError as error:
+        log.failure("Failed to get a job", exc=error)
+        job.failure("Failed to get a job")
 
     # if it's a non empty job continue
     if job:
@@ -66,24 +67,42 @@ def process_next_job():
         using stream 2's timestamp and tick interval
 
         """
+
+        # open each stream as a text to be used in the process
         f = job.dataset.brushing.open_text()
         if not f:
+            log.failure("Stream 2 connect be opened")
             job.failed("Stream 2 cannot be opened")
         g = job.dataset.settings.open_text()
         if not g:
+            log.failure("Stream 1 cannot be opened")
             job.failed("Stream 1 cannot be opened")
+
+        # create dictionaries statistics to hold the data that will be inserted to to the db or saved for each job
         settings = {}
         brush = {'pct': 0, 'cur_tick': 0, 'tooth': 0}
         start = 0
         end = 0
-        z = 1
         time_lost = 0
+
+        # create variables necessary for the process
+        z = 1
         passed = 0
         prev_stamp = 0
         if f:
             x = f.readline()
+
+            # Cannot start with an empty line and will always have a start update with tick 0. If it is empty then the rest of the stream is empty
             if x is None:
+                log.failure("Stream 2 is empty")
                 job.failed("Stream 2 is empty")
+
+            # JSON files have curly braces at the start and end of each entry. If it's true, Stream 2 input is trash input
+            if x[0] != '{' and x[len(x) - 1] != '}':
+                log.failure("Stream 2 input is trash")
+                job.failed("Stream 2 input is trash")
+
+            # iterate through stream 2
             while x:
                 stamp = 0
                 y = 0
@@ -92,6 +111,8 @@ def process_next_job():
                 seen = 0
                 log.debug(x)
                 for idx, i in enumerate(x):
+
+                    # finds '"' and if it's the first one, marks seen as 1 and if the second one, set meas to the key betweeen the two indices
                     if i == '"':
                         if seen:
                             seen = 0
@@ -103,19 +124,22 @@ def process_next_job():
                         seen = 1
                         y = idx
                         log.debug(x[y], i)
+
+                    # if a ',' is found the value of the previous key is found and val is set to that value
                     elif i == "," or i == "}":
                         val = float(x[y: idx])
                         if i == ",":
                             y = idx + 2
                         log.debug(i, x[y], val)
+
+                    # if both meas and val are not empty, enter it in the settings dictionary
                     if meas != "" and val is not None:
                         log.debug(meas, val)
                         settings[meas] = val
                         meas = ""
                         val = None
-                if settings is None:
-                    job.failed("Stream 2 had trash input")
 
+                # Line of Stream 2 has been ingested
                 # stream 1 iterate
                 log.debug("Stream 1 iterate")
 
@@ -127,11 +151,16 @@ def process_next_job():
                     log.info(prev_interval)
 
                 # remove timestamp from dict so no redundancies in db entry
-                strea_2_stamp = settings.pop("timestamp")
+                stream_2_stamp = settings.pop("timestamp")
 
-                # create db entry for the completed stream 2 line
+                # create db entry for the completed stream 2 line. db connect and close are called here to minimize network time
+                try:
+                    db.connect()
+                except IOError as error:
+                    log.failure("Failed to connect to database", exc=error)
+                    job.failure("Failed to connect to database")
                 db.insert("Stream 2", stream_2_stamp, id, settings)
-            
+                db.close()
                 stream_2_tick = str(settings["tick"])
                 log.info(stream_2_tick)
         
@@ -139,20 +168,26 @@ def process_next_job():
                     if passed == 2:
                         log.debug("seen once")
                         log.debug(z)
-                        try:
-                            brush_array = z.split(',')
-                        except:
+                        brush_array = z.split(',')
+
+                        # Each line of the csv always contains the tick, tooth number and percent clean. If the line, after splitting does not contain 3 indices, it is trash data
+                        if len(brush_array) != 3:
+                            log.failure("Stream 1 input is trash")
                             job.failed("Separator in stream not found, trash input")
                         log.debug(brush_array)
                         brush["cur_tick"] = brush_array[0]
                         brush["tooth"] = brush_array[1]
                         brush["pct"] = brush_array[2]
+                    
+                        
                     if int(brush["cur_tick"]) < int(settings["tick"]) and passed == 2:
                         log.debug("current stream 2 tick is larger than current stream 1 tick")
                         # gets timestamp for current tick
                         log.debug("gets timestamp for current tick")
                         stamp = prev_stamp + (float(brush["cur_tick"]) * prev_interval)
                         log.debug(stamp)
+                        
+                        
                         # time between stream 2 timestamp and current timestamp minus the prev tick's timestamp
                         # to get the difference in seconds between the two ticks
                         log.debug("time lost is the sum of difference between two tick's timstamps")
@@ -162,26 +197,45 @@ def process_next_job():
                         prev_stamp = stamp
                         log.debug(prev_stamp)
                         log.debug(brush)
-                        # create db entry for the completed stream 1 line
+                        
+                        
+                        # create db entry for the completed stream 1 line db connect and close are called here to minimize network time
+                        try:
+                            db.connect()
+                        except IOError as error:
+                            log.failure("Failed to connect to database", exc=error)
+                            job.failure("Failed to connect to database")
                         db.insert("Stream 2", stamp, id, brush)
+                        db.close()
                         z = g.readline()
+                    
+                    # stream 1 tick is higher than stream 2 tick
                     elif int(brush["cur_tick"]) >= int(settings["tick"]) and passed == 2:
                         log.info("current tick is larger than stream 2 tick. Didn't call readline so the brush array will not "
                                  "be updated until after the next iteration")
                         break
+                    
+                    # First pass is to make z hold the first line of the CSV containing the titles fo each column. Second pass is for z to hold the first line with wanted info
                     if passed != 2:
                         log.debug("First pass complete z now equals first real line of stream 1")
                         passed += 1
                         z = g.readline()
+
+                        # CSV do not have empty lines. If an empty line is encountered first, the stream is empty
                         if g is None:
+                            log.failure("Stream 1 is empty")
                             job.failed("Stream 1 is empty")
+                
+                
                 prev_interval = settings["tick_interval"]
                 x = f.readline()
-
+        
+        # just in case Stream 2 closes before or during ingesting
         else:
-            log.error("not open")
+            log.failure("Stream 2 is not open")
             job.failed("Stream 2 cannot be opened")
     
+        # gather statistics to be saved
         end = settings["timestamp"]
         log.info("start stamp")
         log.debug(start)
@@ -190,11 +244,12 @@ def process_next_job():
         log.debug(end)
         job.save_statistic("t_end", end)
         job.save_statistic("time_lost", time_lost)
+        
         # close files
         f.close()
         g.close()
-        # close connection to db
-        db.close()
+        
+        # commit job as success
         job.commit()
 
     
