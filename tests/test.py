01fea51b2d6041357f269d042f9fa4f20455e4cc
Jeremiah Nofrada <jnnofrada@gmail.com>
2020-08-04 12:58:41 -0400

    ingested and kept track of start and end time

--- tests/test.py
+++ tests/test.py
@@ -1,42 +1,58 @@
 import io
+import logging
+
+log = logging.getLogger()
+logging.basicConfig(filename= 'tests.log', level=logging.DEBUG)
 print("start")
 
 f = open('settings.jsonlines', 'r', )
 settings = {}
+start = 0
+end = 0
 if f:
     x = f.readline()
-    y = 0
-    meas = ""
-    val = None
-    seen = 0
-    print(x)
-    for idx, i in enumerate(x):
-        if i == '"':
-            if seen:
-                seen = 0
-                meas = x[y + 1: idx]
-                print(meas)
-                y = idx + 3
-                print(i, x[y], meas)
-                continue
-            seen = 1
-            y = idx
-            print(x[y], i)
-        elif i == "," or i == "}":
-            val = float(x[y: idx])
-            if i == ",":
-                y = idx + 2
-            print(i, x[y], val)
-        if meas != "" and val is not None:
-            print(meas, val)
-            settings[meas] = val
-            meas = ""
-            val = None
+    while x:
+        y = 0
+        meas = ""
+        val = None
+        seen = 0
+        log.debug(x)
+        for idx, i in enumerate(x):
+            if i == '"':
+                if seen:
+                    seen = 0
+                    meas = x[y + 1: idx]
+                    log.debug(meas)
+                    y = idx + 3
+                    log.debug(i, x[y], meas)
+                    continue
+                seen = 1
+                y = idx
+                log.debug(x[y], i)
+            elif i == "," or i == "}":
+                val = float(x[y: idx])
+                if i == ",":
+                    y = idx + 2
+                log.debug(i, x[y], val)
+            if meas != "" and val is not None:
+                log.debug(meas, val)
+                settings[meas] = val
+                meas = ""
+                val = None
+
+        if settings["tick"] == 0.0:
+            start = settings["timestamp"]
+
+        log.debug(settings)
+        z = settings["tick"]
+        log.debug(z)
+        
+        x = f.readline()
 
 else:
-    print("not open")
+    log.debug("not open")
     
-print(settings)
-z = settings["tick"]
-print(z)
-
+end = settings["timestamp"]
+log.debug(start)
+log.debug(end)
+f.close()
