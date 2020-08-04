1aeddf7d9933bf01d014ee451e62899c2cf57d29
Jeremiah Nofrada <jnnofrada@gmail.com>
2020-08-04 12:26:09 -0400

    base test for ingesting lines of of stream 2

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,42 @@
 import io
 print("start")
-path = "..\\sample\\settings.jsonlines"
 
-f = open(path, 'r', )
-
+f = open('settings.jsonlines', 'r', )
+settings = {}
 if f:
     x = f.readline()
+    y = 0
+    meas = ""
+    val = None
+    seen = 0
+    print(x)
+    for idx, i in enumerate(x):
+        if i == '"':
+            if seen:
+                seen = 0
+                meas = x[y + 1: idx]
+                print(meas)
+                y = idx + 3
+                print(i, x[y], meas)
+                continue
+            seen = 1
+            y = idx
+            print(x[y], i)
+        elif i == "," or i == "}":
+            val = float(x[y: idx])
+            if i == ",":
+                y = idx + 2
+            print(i, x[y], val)
+        if meas != "" and val is not None:
+            print(meas, val)
+            settings[meas] = val
+            meas = ""
+            val = None
 
 else:
     print("not open")
+    
+print(settings)
+z = settings["tick"]
+print(z)
 
-print(x)
